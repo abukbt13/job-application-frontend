1fefe3c996e96624d06bb5f4d57b279f66931a20
Benalagat <benardkiplagat2000@mail.com>
2023-07-17 15:04:47 +0300

    testing sh

--- src/views/testing/Testing.vue
+++ src/views/testing/Testing.vue
@@ -1,13 +1,21 @@
 <script setup>
 import { ShForm } from '@iankibetsh/shframework'
-
+import {ref} from 'vue'
+const user_id=ref('')
+user_id.value=1
 </script>
 <template>
 <sh-form
-:fields="['subject','book']"
-action="users/add"
+:fields="['title','subject','author']"
+action="add_book"
 
 :successCallback="userRegistered"
+:fill-selects="{
+    user_id: {
+      url: 'show-book/1'
+    }
+  }"
+
 />
 </template>
 <style>
