cb8c4956f1b88ad97c330e5f7fa0a2e2ba569cc4
Freemano The Only <abukbt13>
2023-07-19 18:46:58 +0300

    reset

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,7 @@ import EmploymentExperience from "@/views/applicant/EmploymentExperience.vue";
 import Testing from "@/views/testing/Testing.vue";
 
 import Watch from "@/views/new_concepts/Watch.vue";
+import FinishReset from "@/views/auth/FinishReset.vue";
 
 
 const routes = [
@@ -88,6 +89,12 @@ const routes = [
     path:'/forgot-password',
     name:'forgot-password',
     component:ForgotPassword
+  },
+
+  {
+    path:'/finishreset/:id',
+    name:'finishreset',
+    component:FinishReset
   },
   {
     path:'/dashboard',
